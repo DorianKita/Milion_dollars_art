24070be9e422068e0694e20afcb772ef8d3ae7dc
Dorian Kita <dorian.kita90@gmail.com>
2024-11-20 21:55:12 +0100

    Created list of colors using colorgram module

---
+++ main.py
@@ -0,0 +1,15 @@
+# import colorgram
+# number_of_colors = 20
+# color_list = []
+# colors = colorgram.extract('hirst_complete_spot_paintings_1024x1024@2x.webp', number_of_colors)
+#
+# for color in range(number_of_colors):
+#     r = colors[color].rgb.r
+#     g = colors[color].rgb.g
+#     b = colors[color].rgb.b
+#     color_tuple = (r,g,b)
+#     color_list.append(color_tuple)
+#
+# print(color_list)
+
+final_color_list = [(140, 49, 106), (164, 169, 38), (244, 80, 56), (228, 115, 163), (3, 143, 56), (215, 234, 231), (241, 65, 140), (1, 143, 184), (162, 55, 51), (50, 203, 226), (254, 230, 0), (20, 166, 126), (244, 223, 49), (210, 231, 234), (171, 186, 177), (27, 197, 220), (232, 165, 190)]
